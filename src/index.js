69c8f44791173cd82bf11eabe66b0e425ce42052
Renato De la Rosa <rdelarosa@develovers.com.pe>
2022-06-14 21:03:15 -0500

    JSX

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('app'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//const h1 = React.createElement('h1', null, '🛒 Quatu | Shopping Cart');
+const appName = 'Qhatu';
+const h1 = <h1>🛒 {appName === 'Qhatu' ? '🤔' : '😢'} | Shopping Cart</h1>;
+
+root.render(h1);
